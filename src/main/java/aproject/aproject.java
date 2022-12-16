ba9889512ce97e3fd7ba6c20107c41d6e2a9d70d
seshadrireddy10 <seshadrireddy1492@gmail.com>
2022-12-16 08:35:33 +0530

    first commit

---
+++ src/main/java/aproject/aproject.java
@@ -0,0 +1,12 @@
+package aproject;
+
+public class aproject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		System.out.println("welcome to devops........");
+             
+	}
+
+}
